4f7a5a7e6ed4ffa531530d4c442fdd40ba34c7db
PPeti1999 <pasztoripeti@hotmail.com>
2024-12-03 00:14:54 +0100

    page eredeti

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,13 +14,11 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-wrap gap-4 justify-center p-24">
   {products?.map((product) => (
-    <div className="h-96 flex flex-col border border-black rounded shadow-lg">
     <ProductItem
       key={product.id}
       product={product}
       isSignedIn={isSignedIn}
     />
-    </div>
   ))}
 </main>
   );
