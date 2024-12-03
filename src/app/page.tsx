967c3d649206f08b9598201ce04c6cc393512646
PPeti1999 <pasztoripeti@hotmail.com>
2024-12-03 14:10:00 +0100

    2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default async function Home() {
   // console.log("products", products);
 
   return (
-    <main className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
+    <main className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-12">
   {products?.map((product) => (
     <ProductItem
       key={product.id}
