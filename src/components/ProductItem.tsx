77dfde480e78abe591f3685af93293916b3f900d
PPeti1999 <pasztoripeti@hotmail.com>
2024-12-03 11:33:50 +0100

    1

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -10,6 +10,25 @@ interface ProductItemProps {
 
 const ProductItem: React.FC<ProductItemProps> = ({ product, isSignedIn }) => {
   return (
+    <div className="rounded-lg border border-black flex flex-col justify-between p-4 h-full">
+    
+      {product.image && (
+         <div className="h-48 w-full overflow-hidden rounded-lg mb-4">
+          <ImageComponent path={product.image} altText={product.name} />
+        </div>
+      )}
+       
+        
+       <h2 className="text-xl font-bold mb-2">{product.name}</h2>
+        <div className="text-gray-600">{product.description || "No description available."}</div>
+        <ProductItemControls id={product.id} isSignedIn={isSignedIn} />
+      
+    </div>
+  );
+};
+/*
+const ProductItem: React.FC<ProductItemProps> = ({ product, isSignedIn }) => {
+  return (
     <div className="border-black border flex flex-col h-full">
       {product.image && (
          <div className="flex-shrink-0">
@@ -20,23 +39,6 @@ const ProductItem: React.FC<ProductItemProps> = ({ product, isSignedIn }) => {
         
        <h2 className="text-xl font-bold text-center">{product.name}</h2>
         <div >{product.description || "No description available."}</div>
-        <ProductItemControls id={product.id} isSignedIn={isSignedIn} />
-      </div>
-    </div>
-  );
-};
-/*
-const ProductItem: React.FC<ProductItemProps> = ({ product, isSignedIn }) => {
-  return (
-    <div className="rounded-lg border-black border my-1 p-2 flex gap-2">
-      {product.image && (
-        <div>
-          <ImageComponent path={product.image} altText={product.name} />
-        </div>
-      )}
-      <div>
-        <h2 className="text-xl font-bold">{product.name}</h2>
-        <div>{product.description || "No description available."}</div>
         <ProductItemControls id={product.id} isSignedIn={isSignedIn} />
       </div>
     </div>
